fda7b2925a0493666ff509c72b11974e6c6fbb20
rinana7 <sakairinana@gmail.com>
2026-09-08 12:57:14 -1000

    fix typo when calling the cat frame image and added warning if the image failed to load

--- main.py
+++ main.py
@@ -8,26 +8,41 @@ class TaskWidget(QWidget):
         super().__init__()
         self.setWindowFlags(
             Qt.WindowType.FramelessWindowHint | 
-            Qt.WindowType.WindowStaysOnTopHint | 
-            Qt.WindowType.Tool
+            Qt.WindowType.WindowStaysOnTopHint 
         )
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
 
         self.label = QLabel(self)
-        self.frames = [QPixmap("cat_frame.png"), QPixmap("cat_frame2.png")]
+
+        frame1 = QPixmap("assets/cat-frame.png")
+        frame2 = QPixmap("assets/cat-frame2.png")
+
+        if frame1.isNull():
+            print("Warning: cat_frame1.png not found or failed to load!")
+            self.frames = []
+        else:
+            self.frames = [frame1, frame2]
+
         self.frame_index = 0
 
-        self.x = 100
-        self.y = 800
+        self.x = 200
+        self.y = 200
+        self.move(self.x, self.y)
 
         self.timer = QTimer()
         self.timer.timeout.connect(self.walk)
         self.timer.start(150)
         self.show()
+
+
     def walk(self):
-        self.label.setPixmap(self.frames[self.frame_index])
-        self.frame_index = (self.frame_index+1)%len(self.frames)
-        self.x+=5
+        if self.frames:
+            self.label.setPixmap(self.frames[self.frame_index])
+            self.frame_index = (self.frame_index+1)%len(self.frames)
+
+        self.label.adjustSize()
+        self.adjustSize()
+        self.x+=3
         self.move(self.x, self.y)
 
 if __name__ == '__main__':
