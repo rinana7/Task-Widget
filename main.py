dce4718a262925e9a1055b7296b0243c247d1097
rinana7 <sakairinana@gmail.com>
2026-09-08 13:33:11 -1000

    Put weather fuction outside of class

--- main.py
+++ main.py
@@ -4,12 +4,33 @@ from PyQt6.QtGui import QPixmap
 from PyQt6.QtCore import Qt, QTimer
 import requests
 
+
+LAT="21.3069"
+LON="157.8583"
+def get_weather(lat=LAT, lon=LON):
+    headers = {
+        "User-Agent": "(TaskWidgetApp, taskwidget@example.com)"
+    }
+    try:
+        point_url = f"https://api.weather.gov/points/{lat},{lon}"
+        point_res = requests.get(point_url, headers=headers, timeout=5).json()
+        forecast_url = point_res["properties"]["forecast"]
+
+        forecast_res = requests.get(forecast_url, headers=headers, timeout=5).json
+        current = forecast_res["properties"]["periods"][0]
+
+        return f"{current['temperature']}°{current['temperatureUnit']} • {current['shortForecast']}"   
+    except Exception:
+        return "No connection"
+
+    
 class TaskWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowFlags(
             Qt.WindowType.FramelessWindowHint | 
-            Qt.WindowType.WindowStaysOnTopHint 
+            Qt.WindowType.WindowStaysOnTopHint |
+            Qt.WindowType.Tool
         )
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
 
@@ -48,26 +69,6 @@ class TaskWidget(QWidget):
         self.adjustSize()
         self.x+=3
         self.move(self.x, self.y)
-
-    LAT="21.3069"
-    LON="157.8583"
-    def get_weather(lat=LAT, lon=LON):
-        headers = {
-            "User-Agent": "(TaskWidgetApp, taskwidget@example.com)"
-        }
-        try:
-            point_url = f"https://api.weather.gov/points/{lat},{lon}"
-            point_res = requests.get(point_url, headers=headers, timeout=5).json()
-            forecast_url = point_res["properties"]["forecast"]
-            forecast_res = requests.get(forecast_url, headers=headers, timeout=5).json
-            current_period = forecast_res["properties"]["periods"][0]
-
-            temp = current_period["temperature"]
-            unit = current_period["tempertureUnit"]
-            desc = current_period["shortForecast"]
-            return f"It's {temp}°{unit} & {desc}"
-        except Exception as e:
-            return "No connection"
 
     def update_weather(self):
         msg = get_weather(LAT,LON)
