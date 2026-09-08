58716493fe2a609c152a5fe1423a76d3d5ac4119
rinana7 <sakairinana@gmail.com>
2026-09-08 12:16:27 -1000

    add walking pixel cat sprite with PyQt6 overlay

--- main.py
+++ main.py
@@ -13,9 +13,22 @@ class TaskWidget(QWidget):
         )
         self.setAttricute(Qt.WidgetAttribut.WA_TranslucentBackground)
 
-        self.label = QtLabel(self)
-        self.move(100, 800)
+        self.label = QLabel(self)
+        self.frames = [QPixmap("cat_frame.png"), QPixmap("cat_frame2.png")]
+        self.frame_index = 0
+
+        self.x = 100
+        self.y = 800
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.walk)
+        self.timer.start(150)
         self.show()
+    def walk(self):
+        self.label.setPixmap(self.frames[self.frame_index])
+        self.frame_index = (self.frame_index+1)%len(self.frames)
+        self.x+=5
+        self.move(self.x, self.y)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
